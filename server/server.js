c716b585c08b626dbb260803e095452cae7e2f92
krnkiran22 <145308198+krnkiran22@users.noreply.github.com>
2024-12-12 08:26:51 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ import "express-async-errors";
 
 import dotenv from "dotenv";
 dotenv.config();
-const port = process.env.PORT || 5000;
 
 // ------------DB & AuthenticateUser------------ //
 import connectDB from "./db/connect.js";
@@ -33,41 +32,28 @@ import path from "path";
 
 const __dirname = dirname(fileURLToPath(import.meta.url)); // because we're using ES6-modules not common.js
 
-app.use(express.static(path.resolve(__dirname, "../client/build"))); // static assets
+app.use(express.json()); // make json-data available
+app.use(helmet()); // secure Express-app by setting various HTTP headers
+app.use(xss()); // sanitize user input
+app.use(mongoSanitize()); // prevent MongoDB Operator Injection
 
 if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
 }
 
-app.use(express.json()); // make json-data available
-app.use(helmet()); // secure Express-app by setting various HTTP headers
-app.use(xss()); // node-Connect-middleware to sanitize user input coming from POST body, GET queries, and url params
-app.use(mongoSanitize()); // Sanitizes user-supplied data to prevent MongoDB Operator Injection
-
+// API routes
 app.get("/api/v1", (req, res) => {
-  res.json("Welcome!");
+  res.json({ message: "Welcome!" });
 });
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
-// direct to index.html for react-router after the 2 routes above
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-});
-
+// Middleware
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URL);
-
-    // only connect to server if successfully-connected to DB
-    app.listen(port, () =>
-      console.log(`Server is listening on http://localhost:${port}`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
-};
-start();
+// Export the app
+export default async function handler(req, res) {
+  await connectDB(process.env.MONGO_URL); // Ensure DB is connected
+  return app(req, res); // Pass the request to Express
+}
